f9123cf41bf5cad76c6837920925a511ec550ad5
Jorge Junior <jorge.junior.568@live.com>
2016-03-29 16:00:48 -0300

    Removing all files

--- apagar_nao_funcionou.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once 'fun.php';
-$fun=new fun();
-$fun->conectaMysqli();
-$id=$_GET['id'];
-$fun->executaSQL("DELETE FROM SYS_analytics WHERE md5(id) like '$id'");
-header("Location: verificar_tutoriais");
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(session_destroy()){
-    session_start();
-    $_SESSION['msg']='Logout realizado com sucesso';
-    $_SESSION['cor']='green';
-}
-header("Location: index");
